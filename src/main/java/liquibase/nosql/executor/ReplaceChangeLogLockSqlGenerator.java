a1146de3821ed70432c52bdf636cb6dc2d575bb9
sinan.yumak <sinan.yumak@gmail.com>
2024-07-06 17:01:25 +0300

    LIQU-1: updated replace change lock sql generator.

--- src/main/java/liquibase/nosql/executor/ReplaceChangeLogLockSqlGenerator.java
+++ src/main/java/liquibase/nosql/executor/ReplaceChangeLogLockSqlGenerator.java
@@ -1,16 +1,32 @@
 package liquibase.nosql.executor;
 
 
+import com.mongodb.client.model.Filters;
 import liquibase.database.Database;
 import liquibase.exception.ValidationErrors;
+import liquibase.ext.mongodb.lockservice.MongoChangeLogLock;
+import liquibase.ext.mongodb.lockservice.MongoChangeLogLockToDocumentConverter;
 import liquibase.ext.mongodb.lockservice.ReplaceChangeLogLockStatement;
+import liquibase.ext.mongodb.statement.BsonUtils;
+import liquibase.sql.CallableSql;
 import liquibase.sql.Sql;
 import liquibase.sqlgenerator.SqlGenerator;
 import liquibase.sqlgenerator.SqlGeneratorChain;
 import liquibase.sqlgenerator.core.AbstractSqlGenerator;
+import org.bson.BsonDocument;
+import org.bson.Document;
+
+import java.util.Arrays;
+import java.util.Date;
+
+import static liquibase.ext.mongodb.statement.AbstractRunCommandStatement.COMMAND_NAME;
+import static liquibase.ext.mongodb.statement.AbstractRunCommandStatement.SHELL_DB_PREFIX;
 
 
 public class ReplaceChangeLogLockSqlGenerator extends AbstractSqlGenerator<ReplaceChangeLogLockStatement> {
+
+    public static final int DEFAULT_LOCK_ID = 1;
+
 
     @Override
     public ValidationErrors validate(ReplaceChangeLogLockStatement statement, Database database, SqlGeneratorChain<ReplaceChangeLogLockStatement> sqlGeneratorChain) {
@@ -19,8 +35,36 @@ public class ReplaceChangeLogLockSqlGenerator extends AbstractSqlGenerator<Repla
 
     @Override
     public Sql[] generateSql(ReplaceChangeLogLockStatement statement, Database database, SqlGeneratorChain<ReplaceChangeLogLockStatement> sqlGeneratorChain) {
-        // SY-TODO: create sql sentences for lock operations..
-        return new Sql[0];
+        Document updateCommand = toUpdateCommand(statement);
+        String sqlString = SHELL_DB_PREFIX + COMMAND_NAME
+                + "("
+                + BsonUtils.toJson(updateCommand)
+                + ");";
+
+        CallableSql sql = new CallableSql(sqlString, ";", "1");
+        return new Sql[]{sql};
+    }
+
+    private Document toUpdateCommand(ReplaceChangeLogLockStatement statement) {
+        MongoChangeLogLock lockModel = new MongoChangeLogLock(
+                DEFAULT_LOCK_ID,
+                new Date(),
+                MongoChangeLogLock.formLockedBy(),
+                statement.isLocked()
+        );
+        Document lockDocument = new MongoChangeLogLockToDocumentConverter().toDocument(lockModel);
+
+        BsonDocument idFilter = Filters.eq(MongoChangeLogLock.Fields.id, DEFAULT_LOCK_ID).toBsonDocument();
+
+        Document docToUpdate = new Document();
+        docToUpdate.put("q", Document.parse(idFilter.toJson()));
+        docToUpdate.put("u", lockDocument);
+        docToUpdate.put("upsert", true);
+
+        Document updateCommand = new Document();
+        updateCommand.put("update", statement.getCollectionName());
+        updateCommand.put("updates", Arrays.asList(docToUpdate));
+        return updateCommand;
     }
 
     @Override
